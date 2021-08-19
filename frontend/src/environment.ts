98d94e291dd6c0816955e81021e94b62960d0890
Linh <linhnguyendeveloper@gmail.com>
2021-08-19 23:54:42 +0700

    Fix env + add eth price variable

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,3 +1,6 @@
 // SC address
-const SC_MARKET_CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS || '';
-export default SC_MARKET_CONTRACT_ADDRESS;
+export const SC_CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+
+// Common
+export const ETH_USD_PRICE = process.env.REACT_APP_ETH_USD_PRICE ? Number(process.env.REACT_APP_ETH_USD_PRICE) : 3000;
+
--- frontend/src/utils/getContract.ts
+++ frontend/src/utils/getContract.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import Web3 from 'web3';
-import SC_MARKET_CONTRACT_ADDRESS from 'environment';
+import { SC_CONTRACT_ADDRESS } from 'environment';
 
 export const getContract = async (connector: any) => {
   if (!connector) throw Error('No connector found');
@@ -11,7 +11,7 @@ export const getContract = async (connector: any) => {
 
   // TODO: Get network address whenever JSON file change
   // const contractAddress = contract.networks[1622807330980].address;
-  const contractAddress = SC_MARKET_CONTRACT_ADDRESS;
+  const contractAddress = SC_CONTRACT_ADDRESS;
 
   return new web3.eth.Contract(contractAbi, contractAddress);
 };
