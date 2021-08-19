4a156a0bdb151435f9f8336b083841ead2465998
Linh <linhnguyendeveloper@gmail.com>
2021-08-19 15:00:03 +0700

    Fix comment

--- frontend/src/utils/getContract.ts
+++ frontend/src/utils/getContract.ts
@@ -1,13 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 import Web3 from 'web3';
 
-import contract from './SpaceToken.json';
-
 export const getContract = async (connector: any) => {
   if (!connector) throw Error('No connector found');
   const walletProvider = await connector.getProvider();
   const web3 = new Web3(walletProvider);
-  const contractAbi: any = contract.abi;
+  // const contractAbi: any = contract.abi;
+  const contractAbi: any = {};
 
   // TODO: Get network address whenever JSON file change
   // const contractAddress = contract.networks[1622807330980].address;
