b28e2f383368b29f407be15284f357318431f396
Linh <linhnguyendeveloper@gmail.com>
2021-08-26 10:42:46 +0700

    Add Call SC and get data SC

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,5 +1,5 @@
 // SC address
-export const SC_CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+export const SC_CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS || '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 // Common
 export const ETH_USD_PRICE = process.env.REACT_APP_ETH_USD_PRICE ? Number(process.env.REACT_APP_ETH_USD_PRICE) : 3000;
--- frontend/src/ui/Header/ShoppingBag.tsx
+++ frontend/src/ui/Header/ShoppingBag.tsx
@@ -16,15 +16,24 @@ const ShoppingBag: React.FC = () => {
   const handleCallSC = async () => { 
     if (connector) {
       const contract  = await getContract(connector);
-      console.log(contract)
-      // const result = await contract.methods.SMARTCONTRACT_METHOD //EXAMPLE CALL SC
+      await contract.methods.createOrder('122223',account,10,1,1).send({from:account,type: '0x2',value:11}).on('receipt', async () => {
+        console.log('SUCCESS')
+      })
     }
   }
-
+  const getOrder = async () => {
+    if (connector) {
+      const contract  = await getContract(connector);
+      const result = await contract.methods.getOrderInfo('123').call()
+      console.log({result})
+    }
+    
+  }
   return (
     <Container>
       <Button onClick={handleConnectWallet} $bgType='primary'>{active ? account?.slice(0,10) : 'Connect Wallet'}</Button>
       {active && <Button onClick={handleCallSC} $bgType='primary'>Call SC</Button>}
+      {active && <Button onClick={getOrder} $bgType='primary'>get order</Button>}
 
       <Link to='/cart'>
         <img src={shoppingBag} alt='shoppingBag' width='50px' height='100px' />
